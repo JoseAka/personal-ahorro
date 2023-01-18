8745972aa291854c95f7b95ab4ef032a8e0b8f07
JoseAka <81304802+JoseAka@users.noreply.github.com>
2023-01-18 21:04:43 +0100

    JAGN: Subida inicial

---
+++ src/main/java/personalahorro/jagn/JagnApplication.java
@@ -0,0 +1,13 @@
+package personalahorro.jagn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JagnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JagnApplication.class, args);
+	}
+
+}
